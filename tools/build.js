d3eca26bab0fc3309d0e32f41cba2e12fba5d7cd
Alex Young <alex.young@hive.hr>
2018-04-30 22:25:34 +0100

    Add missing build dependency, fix npm publish
    Adds npmignore file WITHOUT dist being ignored
    Resolves 4

--- tools/build.js
+++ tools/build.js
@@ -1,68 +1,90 @@
-const fs = require('fs');
-const del = require('del');
-const rollup = require('rollup');
-const babel = require('rollup-plugin-babel');
-const uglify = require('rollup-plugin-uglify');
-const pkg = require('../package.json');
+const fs = require("fs");
+const del = require("del");
+const rollup = require("rollup");
+const babel = require("rollup-plugin-babel");
+const uglify = require("rollup-plugin-uglify");
+const pkg = require("../package.json");
 
 const bundles = [
     {
-        format: 'cjs', ext: '.js', plugins: [],
-        babelPresets: ['es2015-rollup', 'stage-1', 'react'], babelPlugins: [
-            'transform-es2015-destructuring',
-            'transform-es2015-function-name',
-            'transform-es2015-parameters',
+        format: "cjs",
+        ext: ".js",
+        plugins: [],
+        babelPresets: ["es2015-rollup", "stage-1", "react"],
+        babelPlugins: [
+            "transform-es2015-destructuring",
+            "transform-es2015-function-name",
+            "transform-es2015-parameters"
         ]
     },
     {
-        format: 'es', ext: '.mjs', plugins: [],
-        babelPresets: ['es2015-rollup', 'stage-1', 'react'], babelPlugins: [
-            'transform-es2015-destructuring',
-            'transform-es2015-function-name',
-            'transform-es2015-parameters'
+        format: "es",
+        ext: ".mjs",
+        plugins: [],
+        babelPresets: ["es2015-rollup", "stage-1", "react"],
+        babelPlugins: [
+            "transform-es2015-destructuring",
+            "transform-es2015-function-name",
+            "transform-es2015-parameters"
         ]
     },
     {
-        format: 'cjs', ext: '.browser.js', plugins: [],
-        babelPresets: ['es2015-rollup', 'stage-1', 'react'], babelPlugins: []
+        format: "cjs",
+        ext: ".browser.js",
+        plugins: [],
+        babelPresets: ["es2015-rollup", "stage-1", "react"],
+        babelPlugins: []
     },
     {
-        format: 'umd', ext: '.js', plugins: [],
-        babelPresets: ['es2015-rollup', 'stage-1', 'react'], babelPlugins: [],
-        moduleName: 'my-library'
+        format: "umd",
+        ext: ".js",
+        plugins: [],
+        babelPresets: ["es2015-rollup", "stage-1", "react"],
+        babelPlugins: [],
+        moduleName: "react-doc-props"
     },
     {
-        format: 'umd', ext: '.min.js', plugins: [uglify()],
-        babelPresets: ['es2015-rollup', 'stage-1', 'react'], babelPlugins: [],
-        moduleName: 'my-library', minify: true
+        format: "umd",
+        ext: ".min.js",
+        plugins: [uglify()],
+        babelPresets: ["es2015-rollup", "stage-1", "react"],
+        babelPlugins: [],
+        moduleName: "react-doc-props",
+        minify: true
     }
 ];
 
 let promise = Promise.resolve();
 
 // Clean up the output directory
-promise = promise.then(() => del(['dist/*']));
+promise = promise.then(() => del(["dist/*"]));
 
 // Compile source code into a distributable format with Babel and Rollup
 for (const config of bundles) {
-    promise = promise.then(() => rollup.rollup({
-        entry: 'src/index.js',
-        external: Object.keys(pkg.dependencies),
-        plugins: [
-            babel({
-                babelrc: false,
-                exclude: 'node_modules/**',
-                presets: config.babelPresets,
-                plugins: config.babelPlugins,
-                runtimeHelpers: true
+    promise = promise.then(() =>
+        rollup
+            .rollup({
+                entry: "src/index.js",
+                external: Object.keys(pkg.dependencies),
+                plugins: [
+                    babel({
+                        babelrc: false,
+                        exclude: "node_modules/**",
+                        presets: config.babelPresets,
+                        plugins: config.babelPlugins,
+                        runtimeHelpers: true
+                    })
+                ].concat(config.plugins)
             })
-        ].concat(config.plugins),
-    }).then(bundle => bundle.write({
-        dest: `dist/${config.moduleName || 'main'}${config.ext}`,
-        format: config.format,
-        sourceMap: !config.minify,
-        moduleName: config.moduleName,
-    })));
+            .then(bundle =>
+                bundle.write({
+                    dest: `dist/${config.moduleName || "main"}${config.ext}`,
+                    format: config.format,
+                    sourceMap: !config.minify,
+                    moduleName: config.moduleName
+                })
+            )
+    );
 }
 
 promise.catch(err => console.error(err.stack));
